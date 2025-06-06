c7f08fc83fd295c82affa2a12ca344f64e20a2e9
Luna Fox <hnkohl@gmail.com>
2025-06-06 16:47:27 -0700

    fix: USA >>>>>

--- config/client.lua
+++ config/client.lua
@@ -1,6 +1,6 @@
 return {
     keybind = 'B',                             -- Keybind to toggle seatbelt (https://docs.fivem.net/docs/game-references/input-mapper-parameter-ids/keyboard/)
-    useMPH = false,                            -- Use MPH instead of KMH
+    useMPH = true,                             -- Use MPH instead of KMH
     minSpeedUnbuckled = 20.0,                  -- Minimum speed to fly through windscreen when seatbelt is off
     minSpeedBuckled = 160.0,                   -- Minimum speed to fly through windscreen when seatbelt is on
     harness = {
